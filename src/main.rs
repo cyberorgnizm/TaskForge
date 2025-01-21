b3aaa9ce9f5927bf84bd4643164f5558fcf591ea
Samuel Adole <15205535+cyberorgnizm@users.noreply.github.com>
2025-01-21 22:02:37 +0100

    Create welcome text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+
+fn welcome() {
+    const VERSION: f64 = 1.0;
+    println!("Welcome to TaskForge! v{:.1} \n", VERSION);
+    println!("Please select from the option provided:");
+    println!("1. Add todo item.");
+    println!("2. Edit todo item.");
+    println!("3. Delete todo item.");
+    println!("4. List todo items. \n");
+    print!("=> ")
+}
+
 fn main() {
     println!("Hello, world!");
 }
