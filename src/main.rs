72b3bc37ad39ad8a6320826d2daf6ca8339e1bb7
Samuel Adole <15205535+cyberorgnizm@users.noreply.github.com>
2025-01-25 21:29:29 +0100

    WIP: Add edit todo function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Write};
 
 #[derive(Debug)]
 struct Task {
+    id: u32,
     title: String,
     is_completed: bool
 }
@@ -13,8 +14,9 @@ impl Task {
     }
 }
 
-fn new_task(title: String, is_completed: bool) -> Task {
+fn new_task(id: u32, title: String, is_completed: bool) -> Task {
     Task {
+        id,
         title,
         is_completed
     }
@@ -44,26 +46,46 @@ fn select_opt(option: &mut String) {
         .expect("Failed to read line.");
 }
 
-fn add_todo() {
+fn add_todo(tasks: &mut Vec<Task>) {
     print!("Enter the name of the task: ");
     let _ = io::stdout().flush();
     let mut title = String::new();
     io::stdin()
         .read_line(&mut title)
         .expect("Failed to read line.");
-    let mut task = new_task(title, false);
+    let total_tasks = tasks.len() as u32;
+    let mut task = new_task(total_tasks + 1, title, false);
 
     println!("Todo Item created!");
-    println!("{}", task.title);
-    println!("Mark as completed(Y/N): {}", task.is_completed);
+    println!("{}. {}", task.id, task.title);
+    tasks.push(task);
+}
 
-    let resp = String::new();
-
-    let resp = resp.trim();
-
-    if resp == "Y" {
-        task.complete();
+fn edit_todo(tasks: &mut Vec<Task>) {
+    println!("Enter the ID of the task to edit from the list below:");
+    for task in tasks {
+        println!("{}. {}", task.id, task.title);
     }
+
+    let mut selected_id = String::new();
+
+    io::stdin()
+    .read_line(&mut selected_id)
+    .expect("Error reading line");
+
+    let selected_id = selected_id.parse().expect("Not a valid number");
+
+    let task = tasks.iter_mut().filter(|t| t.id == selected_id);
+
+    // println!("Mark as completed(Y/N): {}", task.is_completed);
+
+    // let resp = String::new();
+
+    // let resp = resp.trim();
+
+    // if resp == "Y" {
+    //     task.complete();
+    // }
 }
 
 
@@ -78,9 +100,11 @@ fn main() {
         std::process::exit(1);
     });
 
+    let mut task_list = Vec::new();
+
     match option {
-        1 => add_todo(),
-        2 => println!("Option 2 selected!"),
+        1 => add_todo(&mut task_list),
+        2 => edit_todo(&mut task_list),
         3 => println!("Option 3 selected!"),
         4 => println!("Option 4 selected!"),
         _ => println!("Invalid option!"),
